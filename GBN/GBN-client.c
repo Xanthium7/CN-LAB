e76844a66f900665417351a5d9ec61c1a327b279
Xanthium7 <akshhaykmurali@gmail.com>
2026-03-25 13:18:55 +0530

    Refactor Go-Back-N client to improve frame sending and timeout handling

--- GBN/GBN-client.c
+++ GBN/GBN-client.c
@@ -14,67 +14,74 @@ int main() {
     struct sockaddr_in server_addr;
     char buff[MAX];
     int ack = -1, w1 = 0, w2, i;
+    int next_to_send = 0;
 
-    // Set timeout to 3 seconds
+    // 1. Setup Timeout (3 seconds)
     struct timeval timeout;
     timeout.tv_sec = 3;
     timeout.tv_usec = 0;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    // CRITICAL: Enable the timeout for the recv function
-    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY; 
-    server_addr.sin_port = htons(PORT);
-
-    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        printf("Connection failed\n");
+    if (sockfd < 0) {
+        printf("\nSocket creation failed\n");
         return -1;
     }
 
-    printf("Enter number of frames: ");
+    // Apply the timeout to the socket
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        printf("\nConnection to server failed\n");
+        return -1;
+    }
+
+    printf("\n--- Go-Back-N Client Started ---\n");
+    printf("Enter total number of frames to send: ");
     scanf("%d", &nf);
     printf("Enter window size: ");
     scanf("%d", &ws);
 
-    w2 = ws - 1;
+    w2 = ws - 1; // Initial window end
 
     while (w1 < nf) {
-        // Send all frames in the current window
-        for (i = w1; i <= w2 && i < nf; i++) {
-            snprintf(buff, sizeof(buff), "%d", i);
+        // STEP A: Send all frames in the current window that haven't been sent yet
+        while (next_to_send <= w2 && next_to_send < nf) {
+            sprintf(buff, "%d", next_to_send);
             send(sockfd, buff, sizeof(buff), 0);
-            printf("Sent Frame %d\n", i);
+            printf("Sent Frame %d\n", next_to_send);
+            next_to_send++;
         }
 
-        // Wait for ACKs
-        while (1) {
-            memset(buff, '\0', sizeof(buff));
-            int n = recv(sockfd, buff, MAX, 0);
+        // STEP B: Wait for an ACK
+        memset(buff, '\0', sizeof(buff));
+        int n = recv(sockfd, buff, MAX, 0);
 
-            if (n > 0) {
-                ack = atoi(buff);
-                printf("Received ACK %d\n", ack);
+        if (n > 0) {
+            ack = atoi(buff);
+            printf("Received ACK %d\n", ack);
 
-                if (ack >= w1) { // Sliding the window
-                    w1 = ack + 1;
-                    w2 = w1 + ws - 1;
-                    if (w1 >= nf) break; // All frames acknowledged
-                }
-            } else {
-                // n < 0 usually indicates a timeout
-                printf("TIMEOUT: Resending Window [%d to %d]\n", w1, (w2 < nf-1 ? w2 : nf-1));
-                break; // Break the inner loop to resend the window
+            // If it's a valid ACK, slide the window forward
+            if (ack >= w1) {
+                w1 = ack + 1;
+                w2 = w1 + ws - 1;
             }
+        } else {
+            // STEP C: Handle Timeout (The "Go-Back" trigger)
+            printf("TIMEOUT: No ACK received. Resending from Frame %d\n", w1);
+            next_to_send = w1; // Reset our pointer to the start of the window
         }
     }
 
-    printf("\nAll frames sent and acknowledged successfully.\n");
+    printf("\nAll %d frames sent and acknowledged. Closing connection.\n", nf);
+    
+    // Tell the server we are done
     strcpy(buff, "EXIT");
     send(sockfd, buff, sizeof(buff), 0);
-    
+
     close(sockfd);
     return 0;
 }
